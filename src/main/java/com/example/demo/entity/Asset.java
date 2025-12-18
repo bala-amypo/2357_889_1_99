e7b54eb7857749652bf806c430846ad85f94253c
bala-amypo <bala@amypo.in>
2025-12-18 09:19:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Asset.java
+++ src/main/java/com/example/demo/entity/Asset.java
@@ -1,8 +1,16 @@
 package com.example.demo.entity;
 
 public class Asset{
+    @Id
     private long id;
+    @Column(unique=true)
     private String assetTag;
-    private 
+    private String assetName;
+    private LocalDate purchaseDate;
+    @Min()
+    private Double purchaseCost;
+    private String status;
+    private LocalDateTime createdAt;
+
 
     }
