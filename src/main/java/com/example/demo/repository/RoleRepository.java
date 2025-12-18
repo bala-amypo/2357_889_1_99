bdb1efb666919412c710a822453e1adcd0581587
bala-amypo <bala@amypo.in>
2025-12-18 15:39:25 +0000

    User commit

--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.entity.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
