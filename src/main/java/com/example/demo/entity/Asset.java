afc4869c587b471b55086a9e72787723fbb7e7cf
bala-amypo <bala@amypo.in>
2025-12-18 09:24:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/Asset.java
+++ src/main/java/com/example/demo/entity/Asset.java
@@ -1,16 +1,102 @@
 package com.example.demo.entity;
 
-public class Asset{
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Asset {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(unique=true)
+
+    @Column(unique = true)
     private String assetTag;
+
     private String assetName;
+
     private LocalDate purchaseDate;
-    @Min()
+
+    @Min(0)
     private Double purchaseCost;
+
     private String status;
+
     private LocalDateTime createdAt;
 
-
+    
+    public Asset() {
     }
+
+    
+    public Asset(long id, String assetTag, String assetName,
+                 LocalDate purchaseDate, Double purchaseCost,
+                 String status, LocalDateTime createdAt) {
+        this.id = id;
+        this.assetTag = assetTag;
+        this.assetName = assetName;
+        this.purchaseDate = purchaseDate;
+        this.purchaseCost = purchaseCost;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAssetTag() {
+        return assetTag;
+    }
+
+    public void setAssetTag(String assetTag) {
+        this.assetTag = assetTag;
+    }
+
+    public String getAssetName() {
+        return assetName;
+    }
+
+    public void setAssetName(String assetName) {
+        this.assetName = assetName;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Double getPurchaseCost() {
+        return purchaseCost;
+    }
+
+    public void setPurchaseCost(Double purchaseCost) {
+        this.purchaseCost = purchaseCost;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
