19b54de9de9ae50846f66d6f841e5b02b302995d
bala-amypo <bala@amypo.in>
2025-12-18 12:20:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/DepreciationRule.java
+++ src/main/java/com/example/demo/entity/DepreciationRule.java
@@ -1,5 +1,12 @@
 package com.example.demo.entity;
 
 public class DeprecitionRule{
-    
+    @Id
+    private long id;
+    @Column(unique=true)
+    private String vendorName;
+    private String contactEmail;
+    private String phone;
+    private LocalDateTime createdAt;
+
 }
