338a6af3465c1c1c680d785dd8e99d3638db7c6a
bala-amypo <bala@amypo.in>
2025-12-18 12:29:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/AssetLifecycleEvent.java
+++ src/main/java/com/example/demo/entity/AssetLifecycleEvent.java
@@ -1,8 +1,10 @@
 package com.example.demo.entity;
 public class AssetLifecycleEvent{
+    @Id
     private long id;
     private String eventType;
     private String eventDescription;
     private LocalDate evenDate;
-    
+    private LocalDateTime loggedAt;
+
 }
